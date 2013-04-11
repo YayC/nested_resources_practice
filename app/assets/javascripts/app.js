9c23b84764342799f7716745af2f2659d27c6ccb
Apprentice <apprentice@devbootcamp.com>
2013-04-10 17:14:55 -0700

    updated with crazy railscasts link_to_function to add questions/ choices

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,20 +1,29 @@
-// function remove_fields(link) {
-//   $(link).previous("input[type=hidden]").value = "1";
-//   $(link).up(".fields").hide();
-// }
 
-// function add_fields(link, association, content) {
-//   var new_id = new Date().getTime();
-//   var regexp = new RegExp("new_" + association, "g")
-//   $(link).up().insert({
-//     before: content.replace(regexp, new_id)
-//   });
-// }
+  function remove_fields(link) {
+    $(link).prev("input[type=hidden]").val("1");
+    $(link).closest(".fields").hide();
+    if ( $(link).closest('label').text() == "Question") {
+      alert('Question removed!')
+    }
+  }
+
+  function add_fields(link, association, content) {
+    var new_id = new Date().getTime();
+    var regexp = new RegExp("new_" + association, "g")
+    $(link).parent().before(content.replace(regexp, new_id));
+  }
+
 
 $(document).ready(function() {
   $('#show_new_form').on('click', function(e){
     e.preventDefault();
 
-    $('form#new_survey').toggle();
+    $('form#new_survey').toggle(toggleButtonName);
+    function toggleButtonName(){
+      $button = $('#show_new_form');
+      $button.text() === 'New Survey' ? $button.text('Hide Form') : $button.text('New Survey');
+    }
   });
+
+  
 });
